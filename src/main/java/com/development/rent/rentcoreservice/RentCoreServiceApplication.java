038652584b9030d1857472cd845af57cf25b6fc4
Andrei Ivanou1 <andrei_ivanou1@epam.com>
2022-09-19 15:35:02 +0300

    Create base Spring Boot Application

---
+++ src/main/java/com/development/rent/rentcoreservice/RentCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.development.rent.rentcoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentCoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentCoreServiceApplication.class, args);
+	}
+
+}
